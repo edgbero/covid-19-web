3abab665d1614737e93aa55ee1a80111afdae5d0
ebabero <edgar.bero@ralali.com>
2020-05-02 08:41:20 +0700

    Updates

---
+++ precache-manifest.70120c4742c169271015d28952ab6746.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8c31b2fc0053793eb283b84320591b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "231ccf233657be84d81d",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a3ebc3965cb4a675391f",
+    "url": "/static/js/2.5dbab487.chunk.js"
+  },
+  {
+    "revision": "db6251776d83a9b97a8ae2f1e6a03ac3",
+    "url": "/static/js/2.5dbab487.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "231ccf233657be84d81d",
+    "url": "/static/js/main.0549fabc.chunk.js"
+  },
+  {
+    "revision": "8d8d139b190630b6bc42",
+    "url": "/static/js/runtime-main.7ba82ef7.js"
+  },
+  {
+    "revision": "d52ddca5b2c949cdfb123037ddeb0a99",
+    "url": "/static/media/pic.d52ddca5.jpg"
+  }
+]);
